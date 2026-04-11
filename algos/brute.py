94d2b1ee0128e9384fa0b93fe2830df0c4e5fd12
alexasadullin <asadulinsd@gmail.com>
2026-04-11 21:36:44 +0300

    simplify sol

--- algos/brute.py
+++ algos/brute.py
@@ -1,10 +1,6 @@
-def prepare(rectangles):
-    return rectangles
-
-
-def query(prepared, x, y):
+def query(rectangles: list, x: int, y: int) -> int:
     count = 0
-    for x1, y1, x2, y2 in prepared:
+    for x1, y1, x2, y2 in rectangles:
         if x1 <= x < x2 and y1 <= y < y2:
             count += 1
     return count
